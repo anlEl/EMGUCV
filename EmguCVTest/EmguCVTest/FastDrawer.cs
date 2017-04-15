95b6b2371bdbdb168c259544adf80a4fe5df433c
anile <anleladag@gmail.com>
2017-04-15 22:47:38 +0300

    FastDrawer

---
+++ EmguCVTest/EmguCVTest/FastDrawer.cs
@@ -0,0 +1,111 @@
+﻿using Emgu.CV;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.XFeatures2D;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EmguCVTest
+{
+    class FastDrawer : IDisposable
+    {
+        private Mat                         homography;
+        private FastDetector                fastCPU;
+        private VectorOfKeyPoint            modelKeyPoints;
+        private VectorOfKeyPoint            observedKeyPoints;
+        private VectorOfVectorOfDMatch      indices;
+        private BriefDescriptorExtractor    descriptor;
+        private Mat                         mask;
+        private int                         k;
+        private double                      uniquenessThreshold;
+        private Mat                         modelDescriptors;
+        private Mat                         observedDescriptors;
+        private BFMatcher                   matcher;
+        private Mat                         result ;
+        public FastDrawer()
+        {
+            homography          = null;
+            fastCPU             = new FastDetector(15);
+            indices             = new VectorOfVectorOfDMatch();
+            descriptor          = new BriefDescriptorExtractor();
+            k                   = 2;
+            uniquenessThreshold = 0.8;
+            modelDescriptors    = new Mat();
+            observedDescriptors = new Mat();
+            matcher             = new BFMatcher(DistanceType.L2);
+            result              = new Mat();
+        }
+        public Image<Bgr, Byte> Draw(Image<Bgr, Byte> modelImage, Image<Bgr, Byte> observedImage)
+        {
+
+            //extract features from the object image
+            modelKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(modelImage));
+            descriptor.Compute(modelImage, modelKeyPoints, modelDescriptors);
+
+            // extract features from the observed image
+            observedKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(observedImage));
+            descriptor.Compute(observedImage, observedKeyPoints, observedDescriptors);
+
+            if (modelKeyPoints.Size == 0 || observedKeyPoints.Size == 0)
+                return observedImage;
+            matcher.Add(modelDescriptors);
+
+
+            matcher.KnnMatch(observedDescriptors, indices, k, null);
+            mask = new Mat(observedDescriptors.Size, observedDescriptors.Depth, observedDescriptors.NumberOfChannels);
+            mask.SetTo(new MCvScalar(255));
+            Features2DToolbox.VoteForUniqueness(indices, uniquenessThreshold, mask);
+
+
+            int nonZeroCount = CvInvoke.CountNonZero(mask);
+            if (nonZeroCount >= 4)
+            {
+                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+                if (nonZeroCount >= 4)
+                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+            }
+
+            //Draw the matched keypoints
+            Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+            indices, result, new MCvScalar(255, 200, 150), new MCvScalar(0, 0, 255), null, Features2DToolbox.KeypointDrawType.DrawRichKeypoints);
+            Image<Bgr, byte> res = result.ToImage<Bgr, byte>();
+            #region draw the projected region on the image
+            if (homography != null)
+            {  //draw a rectangle along the projected model
+                Rectangle rect = res.ROI;
+                PointF[] pts = new PointF[] {
+                new PointF(rect.Left, rect.Bottom),
+                new PointF(rect.Right, rect.Bottom),
+                new PointF(rect.Right, rect.Top),
+                new PointF(rect.Left, rect.Top)};
+
+                //CvInvoke.ProjectPoints(pts , homography);
+                //homography.ProjectPoints(pts);
+
+                res.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
+            }
+            #endregion
+            modelImage.Dispose();
+            return res;
+        }
+
+        public void Dispose()
+        {
+            k                   = 2;
+            uniquenessThreshold = 0.8;
+            homography          = null;
+            fastCPU             = null;
+            indices             = null;
+            descriptor          = null;
+            modelDescriptors    = null;
+            observedDescriptors = null;
+            matcher             = null;
+            result              = null;
+        }
+    }
+}
+
+
--- EmguCVTest/EmguCVTest/Program.cs
+++ EmguCVTest/EmguCVTest/Program.cs
@@ -1,16 +1,11 @@
 ﻿using Emgu.CV;
-using Emgu.CV.CvEnum;
 using Emgu.CV.Features2D;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV.Util;
 using Emgu.CV.XFeatures2D;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EmguCVTest
@@ -19,86 +14,121 @@ namespace EmguCVTest
     {
         static void Main(string[] args)
         {
-            ImageViewer viewer = new ImageViewer(); //create an image viewer
-            Capture capture = new Capture(); //create a camera captue
-            FastDetector detector = new FastDetector(10);
-            Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
-            {  //run this until application closed (close button click on image viewer)
-                Mat frame   = capture.QueryFrame();
-                VectorOfKeyPoint keypoints = new VectorOfKeyPoint( detector.Detect(frame));
-                Features2DToolbox.DrawKeypoints(frame, keypoints, frame, new Bgr(System.Drawing.Color.Red), Features2DToolbox.KeypointDrawType.Default);
-                viewer.Image = frame; //draw the image obtained from camera
-            });
-            viewer.ShowDialog(); //show the image viewer
-        }
-        public static Image<Bgr, Byte> Draw(Image<Gray, Byte> modelImage, Image<Gray, byte> observedImage)
-        {
-            Mat homography = null;
-
-            FastDetector fastCPU = new FastDetector(15, true);
-            VectorOfKeyPoint modelKeyPoints;
-            VectorOfKeyPoint observedKeyPoints;
-            VectorOfVectorOfDMatch indices = new VectorOfVectorOfDMatch();
-
-            BriefDescriptorExtractor descriptor = new BriefDescriptorExtractor();
-
-            Mat mask;
-            int k = 2;
-            double uniquenessThreshold = 0.8;
-
-            //extract features from the object image
-            modelKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(modelImage));
-            UMat modelDescriptors = new UMat();
-            descriptor.Compute(modelImage, modelKeyPoints, modelDescriptors);
-
-            // extract features from the observed image
-            observedKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(observedImage));
-            UMat observedDescriptors = new UMat();
-            descriptor.Compute(observedImage, observedKeyPoints, observedDescriptors);
-            BFMatcher matcher = new BFMatcher(DistanceType.L2);
-            matcher.Add(modelDescriptors);
-
-            using (VectorOfVectorOfDMatch dist = new VectorOfVectorOfDMatch(observedDescriptors.Rows))
+            try
             {
-                matcher.KnnMatch(observedDescriptors, indices, k, null);
-                //matcher.KnnMatch(observedDescriptors, indices, dist, k, null);
-                mask = new Mat(modelImage.Size,DepthType.Cv8U,modelImage.NumberOfChannels);
-                mask.SetTo(new MCvScalar(255));
-                Features2DToolbox.VoteForUniqueness(dist, uniquenessThreshold, mask);
-            }
+                ImageViewer viewer = new ImageViewer();
+                Capture capture = new Capture(0);
+                //FastDetector detector = new FastDetector(10);
+                Image<Bgr, Byte> lastFrame = null;
 
-            int nonZeroCount = CvInvoke.CountNonZero(mask);
-            if (nonZeroCount >= 4)
+                
+
+                Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+                {
+                    Image<Bgr, Byte> result = null;
+                    FastDrawer drawer = new FastDrawer();
+                    try
+                    {
+                        Mat frame = capture.QueryFrame();
+                        result = frame.ToImage<Bgr, Byte>();
+                        //VectorOfKeyPoint keypoints = new VectorOfKeyPoint(detector.Detect(result));
+                        //Features2DToolbox.DrawKeypoints(result, keypoints, result, new Bgr(System.Drawing.Color.Red), Features2DToolbox.KeypointDrawType.Default);
+                        if (lastFrame != null)
+                            result = drawer.Draw(lastFrame, result);
+                        lastFrame = frame.ToImage<Bgr, Byte>();
+                        viewer.Image = result;
+                        //result.Dispose();
+                    }
+                    catch (Exception ex1)
+                    {
+
+                    }
+                    finally
+                    {
+                        result.Dispose();
+                        drawer.Dispose();
+                    }
+                });
+                    viewer.ShowDialog(); 
+
+            }
+            catch (Exception ex)
             {
-                nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
-                if (nonZeroCount >= 4)
-                    homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+
             }
-
-            //Draw the matched keypoints
-            Image<Bgr, Byte> result = new Image<Bgr, byte>(modelImage.Size);
-                Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
-               indices, result, new MCvScalar(255, 255, 255), new MCvScalar(255, 255, 255), mask, Features2DToolbox.KeypointDrawType.Default);
-
-            #region draw the projected region on the image
-            if (homography != null)
-            {  //draw a rectangle along the projected model
-                Rectangle rect = modelImage.ROI;
-                PointF[] pts = new PointF[] {
-                new PointF(rect.Left, rect.Bottom),
-                new PointF(rect.Right, rect.Bottom),
-                new PointF(rect.Right, rect.Top),
-                new PointF(rect.Left, rect.Top)};
-
-                //CvInvoke.ProjectPoints(pts, homography);
-                //homography.ProjectPoints(pts);
-
-                result.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
-            }
-            #endregion
-
-            return result;
         }
+
+        
+
+        //public static Image<Bgr, Byte> Draw(Image<Bgr, Byte> modelImage, Image<Bgr, Byte> observedImage)
+        //{
+        //    Mat homography = null;
+
+        //    FastDetector fastCPU = new FastDetector(15);
+        //    VectorOfKeyPoint modelKeyPoints;
+        //    VectorOfKeyPoint observedKeyPoints;
+        //    VectorOfVectorOfDMatch indices = new VectorOfVectorOfDMatch();
+
+        //    BriefDescriptorExtractor descriptor = new BriefDescriptorExtractor();
+
+        //    Mat mask;
+        //    int k = 2;
+        //    double uniquenessThreshold = 0.8;
+
+        //    //extract features from the object image
+        //    modelKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(modelImage));
+        //    Mat modelDescriptors = new Mat();
+        //    descriptor.Compute(modelImage, modelKeyPoints, modelDescriptors);
+            
+        //    // extract features from the observed image
+        //    observedKeyPoints = new VectorOfKeyPoint(fastCPU.Detect(observedImage));
+        //    Mat observedDescriptors = new Mat();
+        //    descriptor.Compute(observedImage, observedKeyPoints, observedDescriptors);
+
+        //    if (modelKeyPoints.Size == 0 || observedKeyPoints.Size == 0)
+        //        return observedImage;
+        //    BFMatcher matcher = new BFMatcher(DistanceType.L2);
+        //    matcher.Add(modelDescriptors);
+            
+           
+        //    matcher.KnnMatch(observedDescriptors, indices, k, null);
+        //    mask = new Mat(observedDescriptors.Size, observedDescriptors.Depth, observedDescriptors.NumberOfChannels);
+        //    mask.SetTo(new MCvScalar(255));
+        //    Features2DToolbox.VoteForUniqueness(indices, uniquenessThreshold, mask);
+
+
+        //    int nonZeroCount = CvInvoke.CountNonZero(mask);
+        //    if (nonZeroCount >= 4)
+        //    {
+        //        nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints, indices, mask, 1.5, 20);
+        //        if (nonZeroCount >= 4)
+        //            homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints, observedKeyPoints, indices, mask, 2);
+        //    }
+
+        //    //Draw the matched keypoints
+        //    Mat result = new Mat();
+        //    Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+        //    indices, result, new MCvScalar(255,200,150), new MCvScalar(0, 0, 255),null, Features2DToolbox.KeypointDrawType.DrawRichKeypoints);
+        //    Image<Bgr, byte> res = result.ToImage<Bgr, byte>();
+        //    #region draw the projected region on the image
+        //    if (homography != null)
+        //    {  //draw a rectangle along the projected model
+        //        Rectangle rect = res.ROI;
+        //        PointF[] pts = new PointF[] {
+        //        new PointF(rect.Left, rect.Bottom),
+        //        new PointF(rect.Right, rect.Bottom),
+        //        new PointF(rect.Right, rect.Top),
+        //        new PointF(rect.Left, rect.Top)};
+
+        //        //CvInvoke.ProjectPoints(pts , homography);
+        //        //homography.ProjectPoints(pts);
+
+        //        res.DrawPolyline(Array.ConvertAll<PointF, Point>(pts, Point.Round), true, new Bgr(Color.Red), 5);
+        //    }
+        //    #endregion
+        //    modelImage.Dispose();
+        //    return res;
+        //}
 
     }
 }
